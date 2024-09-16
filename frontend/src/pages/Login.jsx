57ef0d4420dd248d9f849d8ca7069baa6ca5609e
douglasdug <douglas-dug@hotmail.com>
2024-09-16 16:03:50 -0500

    Login modificado

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 export default function Login() {
@@ -57,38 +57,110 @@ export default function Login() {
   };
 
   return (
-    <div className="max-w-xl mx-auto">
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
-      <h2>Login:</h2>
-      <form>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Correo electronico"
-          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
-        />
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="Contraseña"
-          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
-        />
-        <button
-          type="submit"
-          disabled={isLoading}
-          onClick={handleSubmit}
-          className="bg-indigo-500 p-3 rounded-lg block w-full mt-3"
-        >
-          Login
-        </button>
-      </form>
-    </div>
+    <section className="mx-auto max-w-screen-xl px-4 py-1 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-lg text-center">
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+        <h1 className="text-2xl font-bold sm:text-3xl">Login</h1>
+        <p className="mt-4 text-gray-700">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero
+          nulla eaque error neque ipsa culpa autem, at itaque nostrum!
+        </p>
+        <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
+          <form action="/" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+            <div>
+              <label
+                htmlFor="email"
+                className="mb-2 block font-medium text-gray-900 text-left"
+              >
+                Email:
+              </label>
+              <div className="relative">
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Correo electronico"
+                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                />
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-2 block font-medium text-gray-900 text-left"
+              >
+                Password:
+              </label>
+              <div className="relative">
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Contraseña"
+                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                />
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-gray-500">
+                No tienes cuenta?
+                <Link to="/register/" className="underline ml-1">
+                  Regístrese
+                </Link>
+              </p>
+              <button
+                type="submit"
+                disabled={isLoading}
+                onClick={handleSubmit}
+                className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
