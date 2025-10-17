c082c2dc300c687c50ce63d306db3c159bf86f91
douglasdug <douglas-dug@hotmail.com>
2025-10-17 02:40:06 -0500

    Se corregio la configuracion de Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     sourcemap: mode === "development",
     rollupOptions: {
